927822543d7a19a80c85f72847e53b8ca6402d13
WillD78 <70446223+WillD78@users.noreply.github.com>
2020-11-15 18:05:20 -0800

    Add files via upload

---
+++ TicTacToeJava.js
@@ -0,0 +1,5 @@
+function myFunction(id) {
+    document.getElementById('audio').load();
+    document.getElementById('audio').play();
+    document.getElementById(id).style.backgroundImage = "url('https://www.freepngimg.com/thumb/bullet%20hole/2-bullet-shot-hole-png-image-thumb.png')";
+}
